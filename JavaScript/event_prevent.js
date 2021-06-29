edd546d87ab5244f2bd6565ff3b35b6e4f285242
shk07 <seohk107@gmail.com>
2021-06-29 23:22:00 +0900

    event_prevent

--- JavaScript/event_prevent.js
+++ JavaScript/event_prevent.js
@@ -5,6 +5,11 @@ document.addEventListener('wheel', event => {
   //event.preventDefault();
 },  {passive: true});
 // 만약 스크롤을 취소하고 싶다면 passive:false라는 옵션을 사용
+// passive: true일 경우, listener에서 지정한 함수가 preventDefault()를 호출하지 않음을 나타내는 Boolean입니다. 
+// passive listener 가 preventDefault()를 호출하면 user agent는 콘솔 경고를 생성하는 것 외의 작업은 수행하지 않습니다.
+// https://developer.mozilla.org/ko/docs/Web/API/EventTarget/addEventListener
+
+
 checkbox.addEventListener('click', event => {
   console.log('checked');
   event.preventDefault();
