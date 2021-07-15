591314d3459112e4be1cd53ba33164ca47bda1b8
shk07 <seohk107@gmail.com>
2021-07-15 23:32:43 +0900

    setTimer.js

---
+++ JavaScript/setTimer.js
@@ -0,0 +1,22 @@
+const GAME_DURATION_SEC = 5;
+const DOG_COUNT = 7;
+
+let score = 0;
+let timer = undefined;
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      finishGame(DOG_COUNT === score);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
